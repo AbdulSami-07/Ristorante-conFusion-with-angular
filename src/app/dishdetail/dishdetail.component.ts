62338db37bb7fa07aa9d2d1ee7a1c4b346e9fbeb
AbdulSami-07 <sami07israil@gmail.com>
2020-07-26 17:29:10 +0530

    RxJs Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,11 +13,29 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds : string[];
+  prev : string;
+  next : string;
+
   constructor(private dishService: DishService, private route:ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
-    let id : string = this.route.snapshot.params['id'];   /* e.g of route /dishdetail/1 */
-    this.dishService.getDish(id).subscribe( dish => this.dish = dish);
+    // since we observable params so we don't need take snapshoot of params as we can
+    //  get them as params in url changes. 
+
+    this.dishService.getDishIds().subscribe( (dishIds) => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap( (params : Params) => this.dishService.getDish(params['id'])))   /* e.g of route /dishdetail/1 */
+    .subscribe( dish => { 
+      this.dish = dish;
+      this.setPrevNext(dish.id);
+    });
+
+  }
+  
+  setPrevNext(dishId : string){
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1 ) % this.dishIds.length];
+    this.next  = this.dishIds[(this.dishIds.length + index + 1 ) % this.dishIds.length];
   }
 
   goBack() : void {
